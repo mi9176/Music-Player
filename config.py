35fb18b25a59c401aa75b492d6c199bda44e5553
mi9176 <mi9176017279@gmail.com>
2024-09-26 23:05:28 +0530

    config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "7926548341:AAEAvZ-3VvyE8wzqC1RpiCyna0WwSZFfkGM")
+BOT_USERNAME = getenv("BOT_USERNAME", "audioplay_bot")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID","22278010"))
+API_HASH = getenv("API_HASH", "cfe38cba8df4cc60e1b75d4df4f2693c")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
